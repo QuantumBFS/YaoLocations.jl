f81014af7298dcf5158a7a000cce8738b85fd0ae
Roger-luo <rogerluo.rl18@gmail.com>
2020-11-25 13:43:54 -0500

    move from YaoCompiler

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,7 +8,7 @@ using Test
 end
 
 @testset "tuple conversion" begin
-    @test Tuple(Locations(1)) == (1, )
+    @test Tuple(Locations(1)) == (1,)
     @test Tuple(Locations((2, 4, 5))) == (2, 4, 5)
     @test Tuple(Locations(1:4)) == (1, 2, 3, 4)
 end
@@ -16,7 +16,7 @@ end
 @testset "merge locations" begin
     @test merge_locations(Locations(1), Locations(2)) == Locations(1, 2)
     @test merge_locations(Locations(1:3), Locations(5:9)) == Locations((1:3..., 5:9...))
-    @test merge_locations(Locations(1), Locations(2), Locations(4:8)) == Locations((1, 2, 4:8...))    
+    @test merge_locations(Locations(1), Locations(2), Locations(4:8)) == Locations((1, 2, 4:8...))
 end
 
 @testset "location mapping" begin
@@ -27,7 +27,7 @@ end
 
     @test locs[Locations(1)] == locs
     @test locs[Locations(1:1)] == locs
-    @test locs[Locations((1, ))] == locs
+    @test locs[Locations((1,))] == locs
 
     locs = Locations((1, 3, 5))
     @test_throws LocationError locs[Locations(4)]
@@ -46,4 +46,11 @@ end
     @test locs[Locations(2)] == Locations(4)
     @test locs[Locations(1, 3)] == Locations(3, 5)
     @test locs[Locations(1:2)] == Locations(3:4)
+end
+
+@testset "ctrl location mapping" begin
+    locs = Locations(2:5)
+    @test locs[CtrlLocations((1, 2), (0, 1))] == CtrlLocations((2, 3), (0, 1))
+    @test merge_locations(CtrlLocations((1, 2), (0, 1)), CtrlLocations(3, 4)) ==
+          CtrlLocations((1, 2, 3, 4), (0, 1, 1, 1))
 end
